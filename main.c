6e922e8510050e355f828bd44f33618e44e853b2
wilitp <guilledeipola@gmail.com>
2022-03-16 15:16:47 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+// tipo
+struct max_min_result {
+    int max_value;
+    int min_value;
+    unsigned int max_position;
+    unsigned int min_position;
+};
+
+
+// Function signature
+struct max_min_result compute_max_min(int array[], unsigned int length);
+// Implementacion
+struct max_min_result compute_max_min(int array[], unsigned int length)
+{
+    int i = 1;
+    int min;
+    int max;
+    int maxi;
+    int mini;
+    min = array[0];
+    max = array[0];
+    maxi = 0;
+    mini = 0;
+    while(i < length) {
+        if(array[i] > max){
+            max = array[i];
+            maxi = i;
+        }
+        if(array[i] < min){
+            min = array[i];
+            mini = i;
+        }
+        i += 1;
+    }
+    struct max_min_result r = {max,min,maxi,mini};
+    return r;
+}
+
+int main(void) {
+    int array[] = {0, 9, -1, 4};
+    struct max_min_result result = compute_max_min(array, 4);
+    printf("%d", result.max_value); // Imprime 9
+    printf("%d", result.min_value); // Imprime -1
+    printf("%u", result.max_position); // Imprime 1
+    printf("%u", result.min_position); // Imprime 2
+}
+
---
+++ tictactoe.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>  /* exit() y EXIT_FAILURE */
+#include <stdio.h>   /* printf(), scanf()     */
+#include <stdbool.h> /* Tipo bool             */
+
+#include <assert.h>  /* assert() */
+
+#define BOARD_SIZE 3
+#define CELL_MAX (BOARD_SIZE * BOARD_SIZE - 1)
+
+void print_board(char board[BOARD_SIZE][BOARD_SIZE])
+{
+    int cell = 0;
+    printf("\t .................................................\n");
+    for (int row = 0; row < BOARD_SIZE; ++row) {
+        for (int column = 0; column < BOARD_SIZE; ++column) {
+            printf("\t | %d: %c ", cell, board[row][column]);
+            ++cell;
+        }
+        printf("\t | \n");
+        printf("\t .................................................\n");
+    }
+}
+
+char get_winner(char board[BOARD_SIZE][BOARD_SIZE])
+{
+    board = board;
+    char winner = '-';
+    // Checkeo la primera columna
+    if (board[0][0]==board[1][0] && board[0][0] == board[2][0] && board[0][0]=='X' || board[0][0]=='O'){
+        winner = board[0][0];
+    }
+
+    return winner;
+}
+
+bool has_free_cell(char board[BOARD_SIZE][BOARD_SIZE])
+{
+    bool r = false;
+
+    for(int i = 0; r != true && i<BOARD_SIZE;i++){
+        for(int j = 0; r != true && j<BOARD_SIZE;j++){
+            if(board[i][j] == '-'){
+                r = true;
+            }
+        }
+    }
+
+    return r;
+}
+
+int main(void)
+{
+    printf("TicTacToe [InCoMpLeTo :'(]\n");
+
+    char board[BOARD_SIZE][BOARD_SIZE] = {
+        { '-', '-', '-' },
+        { '-', '-', '-' },
+        { '-', '-', '-' }
+    };
+
+    char turn = 'X';
+    char winner = '-';
+    int cell = 0;
+    printf("%d\n", has_free_cell(board));
+    return 0;
+    // Si todavia no hay ganador y quedan espacios libres, seguimos jugando
+    while (winner == '-' && has_free_cell(board)) {
+        print_board(board);
+        printf("\nTurno %c - Elija posición (número del 0 al 8): ",
+               turn);
+        int scanf_result = scanf("%d", &cell);
+        if (scanf_result <= 0) {
+            printf("Error al leer un número desde teclado\n");
+            exit(EXIT_FAILURE);
+        }
+        if (cell >= 0 && cell <= CELL_MAX) {
+            int row = cell / BOARD_SIZE;
+            int colum = cell % BOARD_SIZE;
+            if (board[row][colum] == '-') {
+                board[row][colum] = turn;
+                turn = turn == 'X' ? 'O' : 'X';
+                winner = get_winner(board);
+            } else {
+                printf("\nCelda ocupada!\n");
+            }
+        } else {
+            printf("\nCelda inválida!\n");
+        }
+    }
+    print_board(board);
+    if (winner == '-') {
+        printf("Empate!\n");
+    } else {
+        printf("Ganó %c\n", winner);
+    }
+    return 0;
+}
