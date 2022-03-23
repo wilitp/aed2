a9db3a506d3e195df38a492981e10600e87caaae
wilitp <guilledeipola@gmail.com>
2022-03-22 21:05:56 -0300

    Lab 1 ej 4 listo

---
+++ lab01/ej4/array_helpers.c
@@ -0,0 +1,57 @@
+/* First, the standard lib includes, alphabetically ordered */
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool array_is_sorted(int a[], unsigned int length) 
+{
+  bool r = true;
+  for(unsigned int i=0; i<(length-1);i++) {
+    if(a[i] > a[i+1]) {
+      r = false;
+      break;
+    }
+  }
+  return r;
+}
+
+unsigned int array_from_file(
+  int array[],
+  unsigned int max_size,
+  const char *filepath) 
+{
+  // Abro el archivo
+  FILE *f;
+  f = fopen(filepath, "r");
+  // Consigo su tamanio
+  unsigned int length;
+  fscanf(f, "%u", &length);
+
+  // Corto la length en el max size si este es menor
+  if(max_size < length){
+    length = max_size;
+  }
+
+  // Voy leyendo y agregando los numeros
+  for(unsigned int i=0;i<length;i++){
+    fscanf(f, "%d", &array[i]);
+  }
+  fclose(f);
+  return length;
+
+}
+
+void array_dump(int a[], unsigned int length) {
+  printf("[");
+  for(unsigned int i=0; i<length; i++) 
+  {
+    if(i != length-1 ){
+      printf(" %d,", a[i]);
+    }
+    else {
+      printf(" %d", a[i]);
+    }
+  }
+  printf("]\n");
+}
---
+++ lab01/ej4/array_helpers.h
@@ -0,0 +1,14 @@
+# ifndef EJ3_ARRAY_HELPERS
+# define EJ3_ARRAY_HELPERS
+#include <stdbool.h>
+
+bool array_is_sorted(int a[], unsigned int length);
+
+unsigned int array_from_file(
+  int array[],
+  unsigned int max_size,
+  const char *filepath);
+
+void array_dump(int a[], unsigned int length);
+
+#endif
