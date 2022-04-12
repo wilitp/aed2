b01ca38f6bf7113cceb9e33b731678ef31a629b0
wilitp <guilledeipola@gmail.com>
2022-04-12 19:49:37 -0300

    Lab 3 ej 5

--- lab03/ej5/sort.c
+++ lab03/ej5/sort.c
@@ -12,8 +12,7 @@
 #include "player.h"
 
 bool goes_before(player_t x, player_t y){
-    // completar aquí
-    return true;
+    return x->rank <= y->rank;
 }
 
 bool array_is_sorted(player_t atp[], unsigned int length) {
@@ -24,7 +23,51 @@ bool array_is_sorted(player_t atp[], unsigned int length) {
     return (i == length);
 }
 
+void swap(player_t a[], unsigned int j, unsigned int i){
+  player_t tmp;
+  tmp = a[j];
+  a[j] = a[i];
+  a[i] = tmp;
+}
+
+static unsigned int partition(player_t a[], unsigned int izq, unsigned int der) {
+  unsigned int i = izq+1;
+  unsigned int j = der;
+
+  while (i<=j){
+    // Si los dos estan mal ubicados
+    if(!goes_before(a[i], a[izq]) && !goes_before(a[izq], a[j])) {
+      swap(a, i, j);
+    }
+    else {
+      // Si el de la izquierda esta bien ubicado
+      if(goes_before(a[i], a[izq])) {
+        i++;
+      }
+      // Si el de la derecha esta bien ubicado
+      if(goes_before(a[izq], a[j])) {
+        j--;
+      }
+    }
+  }
+
+  // Finalmente swapeamos el pivot
+  swap(a, izq, j);
+
+  return j;
+}
+
+static void quick_sort_rec(player_t a[], unsigned int izq, unsigned int der) {
+  unsigned int pivot = partition(a, izq, der); 
+  if(pivot>izq) {
+    quick_sort_rec(a, izq, pivot-1);
+  }
+  if(pivot<der) {
+    quick_sort_rec(a, pivot+1, der);
+  }
+}
+
 void sort(player_t a[], unsigned int length) {
-    // completar aquí
+    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
 
