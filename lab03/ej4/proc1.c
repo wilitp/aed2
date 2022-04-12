fb97132bf343df808b1141f84f284cdcf4ad7686
wilitp <guilledeipola@gmail.com>
2022-04-12 19:08:36 -0300

    Lab 3 ej 4

--- lab03/ej4/proc1.c
+++ lab03/ej4/proc1.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 void absolute(int x, int y) {
-    // Completar aqui
+  if (x >= 0) {
+    y = x;
+  } else {
+    y = -x;
+  }
 }
 
 int main(void) {
-    // Completar aqui
-    return 0;
+  int a;
+  int res;
+  res = 0;
+  a = -98;
+  absolute(a, res);
+  printf("%d\n", res);
+  return 0;
 }
 
